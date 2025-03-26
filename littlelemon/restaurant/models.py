c692591424cc764c8553a1aabefcb6790b898cb9
Jack Williams <jdrwilliams5@gmail.com>
2025-03-26 20:16:47 +0200

    Creating Models for the Menu and Booking

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_Guests = models.IntegerField(
+        validators=[MaxValueValidator(6)]
+    )
+    Booking_date = models.DateTimeField()
+
+
+class Menu(models.Model):
+    Title = models.CharField(max_length=255)
+    Price = models.DecimalField( max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
